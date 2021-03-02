0267a31505e7ef40b1907d8c5bdaf481817d637e
Litzy Joffre <ljoffrer@uncc.edu>
2021-03-02 15:37:22 -0500

    PythonBasics 2 - added logic to is_palindrome function

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -7,12 +7,10 @@
 
 # Part A. count_threes
 # Define a function count_threes(n) that takes an int and
-# returns the number of multiples of 3 in the range from 0
-# to n (including n).
 
 def count_threes(n):
-  # YOUR CODE HERE
-    return int(n/3)
+  # YOUR CODE HERE:
+  return int(n/3) #returns number of multiples of 3 in the range drom 0 to n including n
 
 
 
@@ -21,12 +19,11 @@ def count_threes(n):
 # a string s and returns the character that has the longest consecutive repeat.
 def longest_consecutive_repeating_char(s):
   # YOUR CODE HERE:
-
   a=len(s)
-
+  #declaring
   char=1
-  max= 1
   consec=s[0]
+  max= 1
 
   for i in range(0, a-1):
     if s[i] == s[i + 1]:
@@ -42,6 +39,7 @@ def longest_consecutive_repeating_char(s):
     max = char
     consec= s[i]
   return consec
+#returns the character that has the longest consecutive repeat.
 
 
 
@@ -54,6 +52,25 @@ def longest_consecutive_repeating_char(s):
 # forwards. Treat capital letters the same as lowercase ones
 # and ignore spaces (i.e. case insensitive).
 def is_palindrome(s):
-  # YOUR CODE HERE
+  #YOUR CODE HERE:
+  i = 0
+  pa = len(s)-1 #(similar to circles activity)
+ #declare
 
-  return
+  while i <= pa:
+    if s[i] == ' ':
+      i += 1
+      continue
+
+    if s[pa] == ' ':
+      pa -= 1
+      continue
+
+    #Treat capital letters the same as lowercase ones
+    if s[i].lower() != s[pa].lower():
+      return False
+    i += 1
+    pa -= 1
+  return True #returns whether or not that string is a palindrome
+
+
