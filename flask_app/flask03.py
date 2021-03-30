0120367bbfe4cc8f7df95518e16b29d0c69b6270
Litzy Joffre <ljoffrer@uncc.edu>
2021-03-30 13:25:09 -0400

    flask app #3 - added execution path for POST request for new note

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -3,6 +3,7 @@
 # imports
 import os                 # os is used to get environment variables IP & PORT
 from flask import render_template, Flask  # Flask is the web app that we will customize
+from flask import request
 
 app = Flask(__name__)     # create an app
 
@@ -19,25 +20,34 @@ def index():
 
 @app.route('/notes')
 def get_notes():
+    a_user = {'name': 'Litzy', 'email':'mogli@uncc.edu'}
     notes = {1:{'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}}
-    return render_template('notes.html', notes=notes)
+    return render_template('notes.html', notes=notes, user=a_user)
 
 #add a route and function to handle requests to display this view.  Since the purpose of this route is to display
 # the details of one note will still need to utilize our mock list of notes.
 @app.route('/notes/<note_id>')
 def get_note(note_id):
+    a_user = {'name': 'Litzy', 'email':'mogli@uncc.edu'}
     notes = {1:{'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}}
-    return render_template('note.html', note=notes[int(note_id)])
+    return render_template('note.html', note=notes[int(note_id)],user=a_user)
 
 #Let's add a route and function to handle requests to display this view.
-@app.route('/notes/new')
+@app.route('/notes/new', methods=['GET','POST'])
 def new_note():
-    a_user = {'name': 'Mogli', 'email':'mogli@uncc.edu'}
-    return render_template('new.html', user=a_user)
+    #create mock user:
+    a_user = {'name': 'Litzy', 'email':'mogli@uncc.edu'}
+
+    #check method used for request
+    print('request method is',request.method)
+    if request.method == 'POST':
+        return '<h1> POST method used for this request </h1>'
+    else:
+        return render_template('new.html', user=a_user)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
