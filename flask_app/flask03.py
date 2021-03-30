5f636b63074702df4da3943491e861a049cdc044
Litzy Joffre <ljoffrer@uncc.edu>
2021-03-30 14:08:11 -0400

    flask app #3 - saving note to mock list of nodes

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -4,8 +4,13 @@
 import os                 # os is used to get environment variables IP & PORT
 from flask import render_template, Flask  # Flask is the web app that we will customize
 from flask import request
+from flask import redirect,url_for
 
 app = Flask(__name__)     # create an app
+
+notes = {1:{'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
+             2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
+             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}}
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
@@ -21,9 +26,7 @@ def index():
 @app.route('/notes')
 def get_notes():
     a_user = {'name': 'Litzy', 'email':'mogli@uncc.edu'}
-    notes = {1:{'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
-             2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
-             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}}
+
     return render_template('notes.html', notes=notes, user=a_user)
 
 #add a route and function to handle requests to display this view.  Since the purpose of this route is to display
@@ -31,9 +34,7 @@ def get_notes():
 @app.route('/notes/<note_id>')
 def get_note(note_id):
     a_user = {'name': 'Litzy', 'email':'mogli@uncc.edu'}
-    notes = {1:{'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
-             2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
-             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}}
+
     return render_template('note.html', note=notes[int(note_id)],user=a_user)
 
 #Let's add a route and function to handle requests to display this view.
@@ -43,10 +44,22 @@ def new_note():
     a_user = {'name': 'Litzy', 'email':'mogli@uncc.edu'}
 
     #check method used for request
-    print('request method is',request.method)
     if request.method == 'POST':
-        request_data = request.form
-        return f"data: {request_data} !"
+        #get title data
+        title = request.form['title']
+        #get note data
+        text = request.form['noteText']
+        #create date stamp
+        from datetime import date
+        today = date.today()
+        #format date mm/dd/yyyy
+        today = today.strftime("%m-%d-%Y")
+        #get the last ID used and increment by 1
+        id = len(notes)+1
+        #create new note directory
+        notes[id]={'title': title, 'text': text, 'date': today}
+
+        return redirect(url_for('get_notes', name=a_user))
     else:
         return render_template('new.html', user=a_user)
 
