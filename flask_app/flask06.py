e6064d6e57865d051e409824faa8bf0df15c1091
Litzy Joffre <ljoffrer@uncc.edu>
2021-04-20 15:08:19 -0400

    first commit project

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -14,6 +14,8 @@ from models import User as User
 from forms import RegisterForm
 from flask import session
 from forms import LoginForm
+from models import Comment as Comment
+from forms import RegisterForm, LoginForm, CommentForm
 
 app = Flask(__name__)     # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
@@ -59,10 +61,12 @@ def get_notes():
 # the details of one note will still need to utilize our mock list of notes.
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = db.session.query(User).filter_by(email='ljoffrer@uncc.edu').one()
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-
-    return render_template('note.html', note=my_note ,user=a_user)
+    if session.get('user'):
+        my_note = db.session.query(Note).filter_by(id=note_id, user_id=session['user_id']).one()
+        form = CommentForm()
+        return render_template('note.html', note=my_note, user=session['user'], form=form)
+    else:
+        return redirect(url_for('login'))
 
 #Let's add a route and function to handle requests to display this view.
 @app.route('/notes/new', methods=['GET','POST'])
@@ -183,6 +187,24 @@ def logout():
         session.clear()
 
     return redirect(url_for('index'))
+
+#comment:
+@app.route('/notes/<note_id>/comment', methods=['POST'])
+def new_comment(note_id):
+    if session.get('user'):
+        comment_form = CommentForm()
+        # validate_on_submit only validates using POST
+        if comment_form.validate_on_submit():
+            # get comment data
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(note_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+
+        return redirect(url_for('get_note', note_id=note_id))
+
+    else:
+        return redirect(url_for('login'))
 
 
 
--- flask_app/models.py
+++ flask_app/models.py
@@ -8,6 +8,7 @@ class Note(db.Model):
     date = db.Column("date", db.String(50))
 # can create a foreign key
     user_id= db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    comments = db.relationship("Comment", backref="note", cascade="all, delte-orphan", lazy=True)
     def __init__(self, title, text, date, user_id):
         self.title = title
         self.text = text
@@ -21,13 +22,25 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
     notes = db.relationship("Note", backref="user", lazy=True)
-
+    comments = db.relationship("Comment", backref="user", lazy=True)
     def __init__(self, first_name, last_name, email, password):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = password
         self.registered_on = datetime.date.today()
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    content = db.Column(db.VARCHAR, nullable=False)
+    note_id = db.Column(db.Integer, db.ForeignKey("note.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, content, note_id, user_id):
+        self.date_posted = datetime.date.today()
+        self.content = content
+        self.note_id = note_id
+        self.user_id = user_id
 
 
 
